c79ecfc56f26829682a04b173e8d2c7883417da9
João Santos <76254467+joaosantoss12@users.noreply.github.com>
2023-12-02 19:53:31 +0000

    Add files via upload

---
+++ day 1/challenge 1/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define MAX_DIGITS 100
+
+using namespace std;
+
+int main() {
+    int answer = 0;
+    ifstream file;
+    file.open("input.txt");
+
+    string line;
+    string digits[MAX_DIGITS];
+    int index = -1;
+
+    if(!file.is_open()){
+        cout << "Error opening the file!";
+        return -1;
+    }
+
+    if(file.is_open()) {
+        while (!file.eof()) {
+            index = -1;
+            getline(file, line);
+
+            for (int i = 0; i < line.length(); i++) {
+                if (isdigit(line[i])) {
+                    digits[++index] = line[i];
+
+                }
+            }
+
+            string x;
+
+            if (index >= 1)
+                x = digits[0] + digits[index];
+            else if (index == 0)
+                x = digits[0] + digits[0];
+            else
+                x = "0";
+
+            answer += stoi(x);
+
+        }
+    }
+
+    cout << "ANSWER: " + to_string(answer);
+
+    return 0;
+}
