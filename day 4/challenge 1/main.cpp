17c1755dbfda612af9db698b0e118914da5081e4
João Santos <Joo Santos@LAPTOP-LRVKS0GE>
2023-12-04 23:34:42 +0000

    day 4

---
+++ day 4/challenge 1/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    int answer = 0;
+    int currentCardAnswer;
+
+    ifstream file("../../input.txt");
+
+    string line;
+    vector<int> winningNumbers;
+    vector<int> myNumbers;
+
+    string cardID;
+    string nextToken;
+
+    if (!file.is_open()) {
+        cout << "Error opening the file!";
+        return -1;
+    }
+
+    bool foundSeparator;
+    bool foundAnyWinningNumber;
+
+    while (getline(file, line)) {
+        currentCardAnswer = 0;
+        foundSeparator = false;
+        foundAnyWinningNumber = false;
+
+        istringstream iss(line);
+        iss >> cardID >> cardID;
+        cardID.erase(cardID.length()-1);    // delete the ':'
+
+        winningNumbers.clear();
+        myNumbers.clear();
+
+        while(iss >> nextToken){
+            if(nextToken == "|")
+                foundSeparator = true;
+
+            if(isdigit(nextToken[0]) && !foundSeparator){
+                winningNumbers.push_back(stoi(nextToken));
+            }
+            else if(isdigit(nextToken[0]) && foundSeparator){
+                myNumbers.push_back(stoi(nextToken));
+            }
+
+        }
+
+        for(int i=0;i<winningNumbers.size();i++){
+            for(int j=0;j<myNumbers.size();j++){
+                if(winningNumbers[i] == myNumbers[j] && !foundAnyWinningNumber){
+                    currentCardAnswer = 1;
+                    foundAnyWinningNumber = true;
+                }
+                else if(winningNumbers[i] == myNumbers[j] && foundAnyWinningNumber){
+                    currentCardAnswer *= 2;
+                }
+            }
+        }
+
+        answer += currentCardAnswer;
+    }
+
+    cout << "ANSWER: " << answer;
+}
---
+++ day 4/challenge 2/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int answer = 0;
+    int copies;
+
+    ifstream file("../../input.txt");
+
+    string line;
+    vector<string> lines;
+
+    string cardID;
+    string nextToken;
+
+    if (!file.is_open()) {
+        cout << "Error opening the file!";
+        return -1;
+    }
+
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    file.close();
+
+    vector<int> howManyDoesItGenerate(lines.size(), 0);
+    vector<int> howManyCopiesDoesItHave(lines.size(), 1);
+
+    for (int i = 0; i < lines.size(); i++) {
+        istringstream iss(lines[i]);
+
+        vector<int> winningNumbers;
+        vector<int> myNumbers;
+
+        copies = 0;
+
+        iss >> cardID >> cardID;
+        cardID.pop_back(); // remove the trailing ':'
+
+        while (iss >> nextToken && nextToken != "|") {
+            if (isdigit(nextToken[0])) {
+                winningNumbers.push_back(stoi(nextToken));
+            }
+        }
+
+        while (iss >> nextToken) {
+            if (isdigit(nextToken[0])) {
+                myNumbers.push_back(stoi(nextToken));
+            }
+        }
+
+        for (int k = 0; k < winningNumbers.size(); k++) {
+            for (int j = 0; j < myNumbers.size(); j++) {
+                if (winningNumbers[k] == myNumbers[j]) {
+                    ++copies;
+                }
+            }
+        }
+
+        for (int x = 0; x < copies && (stoi(cardID) + x) < lines.size(); x++) {
+            howManyCopiesDoesItHave[stoi(cardID) + x] += 1 * howManyCopiesDoesItHave[stoi(cardID)-1]; // next cards have one more copy
+            howManyDoesItGenerate[stoi(cardID) - 1] += 1;    // current card generated another copy
+        }
+    }
+
+    for (int y = 0; y < lines.size(); y++) {
+        answer += howManyCopiesDoesItHave[y];
+        //cout << endl << "howManyCopiesDoesItHave[" << y+1 << "]: " << howManyCopiesDoesItHave[y] <<  " howManyDoesItGenerate[" << y+1 << "]: " << howManyDoesItGenerate[y] << endl;
+    }
+
+    cout << "ANSWER: " << answer;
+}
